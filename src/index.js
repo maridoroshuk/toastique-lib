363dc89dd8df279ec2424c05f761aa120ade2a9e
Maryna Darashuk <m.doroshuk@bk.ru>
2022-10-26 14:53:52 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from 'components/App';
+import Toastique from '@/components/Basic/Toastique';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// eslint-disable-next-line import/prefer-default-export
+export { Toastique };
