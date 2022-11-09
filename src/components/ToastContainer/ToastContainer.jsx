94cd42c6058cd50abf1f5ef1931dfe5bd3661fe9
Maryna Darashuk <m.doroshuk@bk.ru>
2022-11-09 09:31:52 +0300

    refactor:  ToastContainer

--- src/components/ToastContainer/ToastContainer.jsx
+++ src/components/ToastContainer/ToastContainer.jsx
@@ -1,66 +1,23 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { createPortal } from 'react-dom';
-import PropTypes from 'prop-types';
+import React, { useRef } from 'react';
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
-import Toast from '@/components/Toast/Toast';
-import useToastAutoClose from '@/hooks/useToastAutoClose';
-import useToastPortal from '@/hooks/useToastPortal';
-import { Container } from './styled';
+import Button from '@/components/Button/Button';
+import ToastPortal from '@/components/ToastPoartal/ToastPortal';
 
-function ToastContainer({ position, autoCloseTime, toasts }) {
-  const [toastList, setToastList] = useState([toasts]);
-  const { loaded, portalId } = useToastPortal(position);
+function ToastContainer(args) {
+  const toastRef = useRef();
 
-  const removeToast = (id) => useCallback(() => {
-    const toastListItem = toastList.findIndex(
-      (t) => t.id === id,
-    );
-    const toastsItem = toasts.findIndex((t) => t.id === id);
-    setToastList(toastList.splice(toastListItem, 1));
-    toasts.splice(toastsItem, 1);
-  }, []);
+  const handleShowToastClick = (properties) => {
+    toastRef.current.addToasts(properties);
+  };
 
-  useEffect(() => {
-    setToastList([...toasts]);
-  }, [toasts]);
-
-  useToastAutoClose(toasts, removeToast, autoCloseTime);
-
-  return loaded
-    ? createPortal(
-      <ErrorBoundary>
-        <Container>
-          {toasts.map((t) => (
-            <Toast
-              key={t.id}
-              toast={t}
-              onCloseToastClick={removeToast}
-            />
-          ))}
-        </Container>
-      </ErrorBoundary>,
-      document.getElementById(portalId),
-    )
-    : null;
+  return (
+    <ErrorBoundary>
+      <ToastPortal ref={toastRef} {...args} />
+      <Button
+        handleOnShow={() => handleShowToastClick(args)}
+      />
+    </ErrorBoundary>
+  );
 }
-
-const toastsPropsType = PropTypes.shape({
-  animation: PropTypes.string,
-  autoCloseTime: PropTypes.number,
-  color: PropTypes.string,
-  content: PropTypes.string,
-  gap: PropTypes.string,
-  heading: PropTypes.string,
-  icon: PropTypes.string,
-  id: PropTypes.string,
-  position: PropTypes.string,
-  variant: PropTypes.string,
-});
-
-ToastContainer.propTypes = {
-  autoCloseTime: PropTypes.number.isRequired,
-  position: PropTypes.string.isRequired,
-  toasts: PropTypes.arrayOf(toastsPropsType).isRequired,
-};
 
 export default ToastContainer;
--- src/components/ToastContainer/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-import { spaces } from '@/theme/sizes';
-
-export const Container = styled.div`
-  gap: ${spaces.xxs};
-  display: flex;
-  flex-direction: column;
-`;
